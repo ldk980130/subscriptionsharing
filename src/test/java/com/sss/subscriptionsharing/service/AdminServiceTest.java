8ef51803ebab28a1a86040d7d052c55e3cb48a01
DK <ldk980130@gmail.com>
2021-11-23 12:16:11 +0900

    test: AdminService 사용자 취소 예외 테스트

--- src/test/java/com/sss/subscriptionsharing/service/AdminServiceTest.java
+++ src/test/java/com/sss/subscriptionsharing/service/AdminServiceTest.java
@@ -2,6 +2,7 @@ package com.sss.subscriptionsharing.service;
 
 import com.sss.subscriptionsharing.domain.user.Status;
 import com.sss.subscriptionsharing.domain.user.User;
+import com.sss.subscriptionsharing.exception.NoAuthorityException;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.PostConstruct;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.Assert.*;
@@ -40,4 +43,31 @@ public class AdminServiceTest {
         assertThat(user.getStatus()).isEqualTo(Status.NORMAL);
     }
 
+    @Test(expected = NoAuthorityException.class)
+    public void notAdminSuspension() throws Exception {
+        //given
+        User user = userService.join("ldk", "1234", "이동규",
+                "dk", "안녕", "ldk980130@gmail.com");
+        User admin = adminService.joinAdmin("admin", "2345", "관리자",
+                "ad", "", "1234@gmail.com");
+
+        //when
+        adminService.userSuspension(user.getId(), admin.getId());
+
+        //then
+        fail();
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void noExistUserSuspension() throws Exception {
+        //given
+        User admin = adminService.joinAdmin("admin", "2345", "관리자",
+                "ad", "", "1234@gmail.com");
+
+        //when
+        adminService.userSuspension(admin.getId(), 100000L);
+
+        //then
+        fail();
+    }
 }
