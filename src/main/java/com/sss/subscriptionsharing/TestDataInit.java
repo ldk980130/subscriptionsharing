038f8fbc4a515a19323c167af83ec312c1421b6d
DK <ldk980130@gmail.com>
2021-12-02 15:47:34 +0900

    fix : 카테고리 정보 DB에 넣고 실행할 때마다 값 있는지 확인

--- src/main/java/com/sss/subscriptionsharing/TestDataInit.java
+++ src/main/java/com/sss/subscriptionsharing/TestDataInit.java
@@ -1,24 +1,60 @@
 package com.sss.subscriptionsharing;
 
-import com.sss.subscriptionsharing.domain.user.User;
-import com.sss.subscriptionsharing.service.UserService;
+import java.util.List;
+
+import com.sss.subscriptionsharing.domain.Category;
+import com.sss.subscriptionsharing.repository.BoardRepository;
+import com.sss.subscriptionsharing.repository.CategoryRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 
 @Component
+@Slf4j
 @RequiredArgsConstructor
 public class TestDataInit {
 
-    private final UserService userService;
+    private final BoardRepository boardRepository;
+    private final CategoryRepository categoryRepository;
 
     @PostConstruct
-    public void init() {
-//        User user1 = userService.join("ldk", "1234", "이동규",
-//                "dk", "안녕", "ldk980130@gmail.com");
-//
-//        User user2 = userService.join("abc", "12344", "김철수",
-//                "ch", "안녕", "abc1234@gmail.com");
-    }
+	public void dataInItCheck() {
+		List<Category> categories = categoryRepository.findAll();
+
+		for (Category category : categories) {
+			log.info("categoryId={}, categoryName={}",category.getId(), category.getName());
+		}
+	}
+
+    // @PostConstruct
+    // public void categoryInit() {
+	//
+    //     Board watcha = Board.create("왓챠");
+    //     boardRepository.save(watcha);
+	//
+    //     categoryRepository.save(Category.create("자유 게시판", watcha));
+    //     categoryRepository.save(Category.create("구인 게시판", watcha));
+    //     categoryRepository.save(Category.create("게시판 추천 게시판", watcha));
+	//
+    //     Board wavve = Board.create("웨이브");
+    //     boardRepository.save(wavve);
+	//
+    //     categoryRepository.save(Category.create("자유 게시판", wavve));
+    //     categoryRepository.save(Category.create("구인 게시판", wavve));
+    //     categoryRepository.save(Category.create("게시판 추천 게시판", wavve));
+	//
+    //     Board spotify = Board.create("스포티파이");
+    //     boardRepository.save(spotify);
+	//
+    //     categoryRepository.save(Category.create("자유 게시판", spotify));
+    //     categoryRepository.save(Category.create("구인 게시판", spotify));
+    //     categoryRepository.save(Category.create("게시판 추천 게시판", spotify));
+	//
+    //     Board none = Board.create("none");
+    //     boardRepository.save(none);
+    //     categoryRepository.save(Category.create("OTT 추천 게시판", none));
+    // }
 }
