4161d7dbacf16ee4f36cf11f496174af7efda930
DK <ldk980130@gmail.com>
2021-12-02 16:28:52 +0900

    feat : 로그인 구현

---
+++ src/main/java/com/sss/subscriptionsharing/web/controller/LoginController.java
@@ -0,0 +1,48 @@
+package com.sss.subscriptionsharing.web.controller;
+
+import java.util.Map;
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.http.HttpRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sss.subscriptionsharing.domain.user.User;
+import com.sss.subscriptionsharing.service.LoginService;
+import com.sss.subscriptionsharing.service.UserService;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+public class LoginController {
+
+	static final String LOGIN_USER = "loginUser";
+
+	private final UserService userService;
+	private final LoginService loginService;
+
+	@PostMapping("/api/login")
+	public ResponseEntity login(@RequestBody Map<String, String> loginForm, HttpServletRequest request) {
+
+		Optional<User> loginUser = loginService.login(loginForm.get("loginId"), loginForm.get("password"));
+
+		if (loginUser.isEmpty()) {
+			return ResponseEntity.badRequest().build();
+		}
+
+		HttpSession session = request.getSession();
+		session.setAttribute(LOGIN_USER, loginUser.get().getLoginId());
+
+		log.info("로그인 성공={}", session.getAttribute(LOGIN_USER));
+
+		return ResponseEntity.ok().build();
+	}
+}
